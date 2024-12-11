396d5f1c6d5c82636b64a0ed0c9fb7e2245d3ef9
Javohir Xatamov <khatamov37@gmail.com>
2024-12-11 18:49:28 +0500

    Add files via upload
    template for functions, use this to test your function

---
+++ functionTester.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+/*import your function here*/
+
+
+int main(){
+
+char *output = "we did not get your output";
+
+/*output = function(argument1, argument2) call your function here*/
+
+printf("output: %s\n", output);
+
+return 0;
+}
