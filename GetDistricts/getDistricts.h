11155cab3b18693a6a7c781b6c2e46e565465007
Davlatbek Kobiljonov <117518580+Davlatbek0710@users.noreply.github.com>
2024-12-14 17:27:33 +0500

    Delete GetDistricts directory

--- GetDistricts/getDistricts.h
+++
@@ -1,9 +0,0 @@
-#ifndef GETDISTRICTS_H
-#define GETDISTRICTS_H
-
-#include <mysql/mysql.h>
-
-char* getDistricts(MYSQL *conn, const char *regionName); // Ensure this matches your implementation
-
-#endif
-
