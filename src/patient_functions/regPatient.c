5c609f52b5659d5cee95d4ff7739225547a8145f
Davlatbek <kobiljonovdavlatbek@gmail.com>
2024-12-14 17:49:08 +0500

    Added regPatient.c and regPatient.h files

--- src/patient_functions/regPatient.c
+++ src/patient_functions/regPatient.c
@@ -6,7 +6,6 @@
 
 // Function to register a patient and return the auto-incremented PatientID
 // If the patient with the same Name and PhoneNumber exists, return "exists" instead 
-// Done by Davlatbek Kobiljonov
 char* regPatient(MYSQL *conn, const char *name, const char *dob, const char *address, 
                  const char *phoneNumber, const char *email, const char *passportID, 
                  const char *citizenship, const char *password) {
