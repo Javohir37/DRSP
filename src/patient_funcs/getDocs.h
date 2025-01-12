4db45951fe37ca4b7e4ee2b6b92f3533c4ca3f90
Javohir37 <j.xatamov@student.inha.uz>
2025-01-12 13:54:25 +0500

    new updated files

--- src/patient_funcs/getDocs.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_DOCS_H
-#define GET_DOCS_H
-
-#include <mysql/mysql.h>
-
-char* getDocs(MYSQL *conn, int hospitalID, const char *spec);
-
-#endif
-
--- src/patient_funcs/patientLogin.h
+++
@@ -1,9 +0,0 @@
-#ifndef PATIENT_LOGIN_H
-#define PATIENT_LOGIN_H
-
-#include <mysql/mysql.h>
-
-char* patientLogin(MYSQL *conn, int patientID, const char *password);
-
-#endif // PATIENT_LOGIN_H
-
